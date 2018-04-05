2543e8b62e4eb41264bdd27127627e77f6a9a877
Jimmy Nam Nguyen <nguyen.jimmy.nam@gmail.com>
2018-04-05 10:58:21 +0200

    Yeet
    yeeet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutMeComponent } from './content/about-me/about-me.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './content/contact/contact.component';
+import { ProjectsComponent } from './content/projects/projects.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutMeComponent,
+    HeaderComponent,
+    ContactComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
